4c5e6d1506461ca10afd3f4a840b6c8f8a7a94a8
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-08-20 21:00:03 +0100

    style about us page

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -4,9 +4,10 @@ export default function addAboutUs () {
     const img = document.createElement('img');
     const para = document.createElement('p');
     head.textContent = "Family Restaurant";
-    img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaKpX93OKgLOEMnJ7SQX6A46oMasmVVBtFLA&usqp=CAU');
+    img.setAttribute('src', 'https://108w864esodx40rbop37tsnk-wpengine.netdna-ssl.com/wp-content/uploads/2020/08/Tupelo-Grand-Opening1.png');
     img.setAttribute('alt', 'image of a restaurant');
     para.textContent = "Our restaurant has been around for 5 years. And with welcoming and open arms we welcome everyone to come and visit. Enjoy the mountain breeze and the good view.";
+    img.setAttribute('id','about-us-img')
     content.appendChild(head)
     content.appendChild(img)
     content.appendChild(para)
