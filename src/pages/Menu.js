920265240c35c6efcbbe2cf0003dd374a5e72cdb
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-08-26 13:23:03 +0100

    style menu section

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,9 +1,16 @@
 const content = document.querySelector('#content');
 const body = document.querySelector('body');
+let header = 0;
 
 export default function addMenuOption(plateImg,plateName) {
     body.className = "menu";
     content.className = "menu_content";
+    if (header % 6 === 0) {
+        const menuHeader = document.createElement('h2');
+        menuHeader.textContent = "Menu";
+        content.appendChild(menuHeader);
+    }
+    header++;
     const menuOption = document.createElement('div');
     menuOption.classList.add("menu-option");
     menuOption.setAttribute('id',plateName)
@@ -14,7 +21,8 @@ export default function addMenuOption(plateImg,plateName) {
     myImage.id = plateName;
     myImage.alt = `A picture of ${plateName}`;
     menuOption.appendChild(myImage);
-    const caption = document.createElement('figcaption');
+    const caption = document.createElement('div');
     caption.textContent = plateName;
+    caption.className = 'caption';
     menuOption.appendChild(caption);
 }
